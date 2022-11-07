a435192acc1834d887cf69c27c754c722c9765db
kzk-maeda <kzk.maeda0711@gmail.com>
2022-11-07 21:02:42 +0900

    get path from args

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func getArgs() string {
+	args := os.Args
+	// fmt.Println(args)
+	if len(args) != 2 {
+		fmt.Println("[ERROR] Set path to variables.tf as arg.")
+		os.Exit(1)
+	}
+	return args[1]
+}
+
 func main() {
-	src := "files/variables.tf"
+	// src := "files/variables.tf"
+	src := getArgs()
 	// ソースファイルに対するバックアップを作成
 	createBackupFile(src)
 	// ソースファイルから空行を削除したtmpファイルを作成
